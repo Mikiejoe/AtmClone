fa3eb5d28eebad58ee57f44364fca7eb9a6aafcb
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-07-29 20:23:56 +0300

    add dashboard

--- src/main/java/com/example/demo/AtmApp.java
+++ src/main/java/com/example/demo/AtmApp.java
@@ -7,13 +7,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AtmApp extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        Scene scene = new Scene(loader.load());
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("ATM Application");
-        primaryStage.show();
+@Override
+    public void start(Stage stage) throws Exception {
+    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signUp.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    stage.setTitle("Hello!");
+    stage.setScene(scene);
+    stage.show();
+//    URL fxmlLocation = getClass().getClassLoader().getResource("signUp.fxml");
+//
+//        FXMLLoader loader = new FXMLLoader(fxmlLocation);
+//        Parent root = loader.load();
+//
+//        primaryStage.setScene(new Scene(root, 520, 400));
+//        primaryStage.setTitle("ATM Application");
+//        primaryStage.show();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/DashboardController.java
@@ -0,0 +1,93 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+    private User authUser;
+
+    @FXML
+    private Button quitButton;
+    @FXML
+    private Button transferButton;
+    @FXML
+    private Button viewTransactionHistoryButton;
+    @FXML
+    private Button withdrawButton;
+    @FXML
+    private Button depositButton;
+    @FXML
+    private Label welcomeLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    public void setAuthUser(User authUser) {
+        this.authUser = authUser;
+        updateUI();
+    }
+
+    private void updateUI() {
+        if (authUser != null) {
+            welcomeLabel.setText("Welcome, " + authUser.getFirstName() + "!");
+        }
+    }
+
+    @FXML
+    private void handleQuitButtonAction() {
+        System.out.println("Quit button pressed");
+    }
+
+    @FXML
+    private void handleTransferButtonAction() {
+        System.out.println("Transfer button pressed");
+    }
+
+    @FXML
+    private void handleTransactionHistoryButtonAction() {
+        System.out.println("View transaction history button pressed");
+        loadTransactionHistoryPage();
+    }
+
+    @FXML
+    private void handleWithdrawButtonAction() {
+        System.out.println("Withdraw button pressed");
+    }
+
+    @FXML
+    private void handleDepositButtonAction() {
+        System.out.println("Deposit button pressed");
+
+    }
+
+    private void loadTransactionHistoryPage() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/transactionHistory.fxml"));
+            Parent root = loader.load();
+
+            TransHistoryController controller = loader.getController();
+            if (controller != null) {
+                controller.setAuthUser(authUser);
+            } else {
+                System.err.println("Failed to load TransHistoryController");
+            }
+
+            Stage stage = (Stage) viewTransactionHistoryButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
